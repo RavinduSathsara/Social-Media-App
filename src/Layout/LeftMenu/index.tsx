0995dee7b0fb98c25559f366564931e1a6d680c3
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-07-17 12:19:28 +0530

    nav fixed

--- src/Layout/LeftMenu/index.tsx
+++ src/Layout/LeftMenu/index.tsx
@@ -8,47 +8,45 @@ import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import { Grid, Button, Typography } from "@mui/material";
 
 const style = {
-  width: "100%",
-  maxWidth: 360,
-  margin: 15,
+  margin: 5,
 };
 
 export default function LeftMenus() {
   return (
     <Grid container sx={style}>
-      <Grid item xs={12}>
+      <Grid item>
         <Button>
-          <HomeIcon sx={{ mr: 2 }} color="primary" />
+          <HomeIcon color="primary" />
           <Typography variant="h6">Home</Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
         <Button>
-          <NotificationsIcon sx={{ mr: 2 }} color="primary" />
+          <NotificationsIcon color="primary" />
           <Typography variant="h6">Notifications</Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
         <Button>
-          <EmailIcon sx={{ mr: 2 }} color="primary" />
+          <EmailIcon color="primary" />
           <Typography variant="h6">Messages</Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
         <Button>
-          <BookmarkIcon sx={{ mr: 2 }} color="primary" />
+          <BookmarkIcon color="primary" />
           <Typography variant="h6">Bookmarks</Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
         <Button>
-          <FormatListBulletedIcon sx={{ mr: 2 }} color="primary" />
+          <FormatListBulletedIcon color="primary" />
           <Typography variant="h6">Lists</Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
         <Button>
-          <PersonIcon sx={{ mr: 2 }} color="primary" />
+          <PersonIcon color="primary" />
           <Typography variant="h6">Profile</Typography>
         </Button>
       </Grid>
